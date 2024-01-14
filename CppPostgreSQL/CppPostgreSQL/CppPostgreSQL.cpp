8826e4374c96edea11bb54bbebfa0c4bad4b1ec4
Egor Levashov <112577182+sxfour@users.noreply.github.com>
2024-01-14 13:29:37 +0300

    Add files via upload

---
+++ CppPostgreSQL/CppPostgreSQL/CppPostgreSQL.cpp
@@ -0,0 +1,30 @@
+﻿#pragma once
+
+#include <string>
+#include <iostream>
+#include <pqxx/pqxx>
+
+int main()
+{
+    std::string connectionString = "host=localhost port=5432 dbname=CppConnector user=postgres password =24e8qd";
+
+    try
+    {
+        pqxx::connection connectionObject(connectionString.c_str());
+
+        pqxx::work worker(connectionObject);
+
+        pqxx::result response = worker.exec("SELECT * FROM testy");
+
+        for (size_t i = 0; i < response.size(); i++)
+        {
+            std::cout << "id: " << response[i][0] << " username: " << response[i][1] << " user_uuid: " << response[i][2] << " password: " << response[i][3] << std::endl;
+        }
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+
+    system("pause");
+}
